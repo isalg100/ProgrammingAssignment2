930154e498302b38683f0422296b2431a134ba8b
isalg100 <is_blk@yahoo.fr>
2020-05-09 07:54:32 +0100

    my first commit of the progrmming ssigment

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
+#ell to Write somme comments discribing this function ! 
+#this function is similr to the mke vector nd che vector itch re just the sme the only chngement i hd done 
+# to replce object of type Vector() by Object of type mterix nd the men function by the  
+#solve function, this is my first submission of this home ork nd i hope its is correcte.
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setcachematrix <- function(solve) m <<- solve
+  getcachematrix <- function() m
+  list(set = set, get = get,
+       setcachematrix = setcachematrix,
+       getcachematrix = getcachematrix)
 
 }
 
@@ -12,4 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getcachematrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setcachematrix(m)
+  m
 }
